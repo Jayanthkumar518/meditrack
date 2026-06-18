b342f0144d38ae88da902bbde47616c4fbd84781
unknown <jayanthkumarbalaka@gmail.com>
2026-06-18 20:05:35 +0530

    crucial updates

--- db.py
+++ db.py
@@ -1,10 +1,14 @@
+import os
 import pymysql
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     return pymysql.connect(
-        host="my-database.c9gayi8ggbbd.ap-south-1.rds.amazonaws.com",
-        user="admin",
-        password="jayanth123",
-        database="stdmanagement",
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
         cursorclass=pymysql.cursors.DictCursor
     )
